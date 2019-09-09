c3b4f80bcfe7b38ddb9865d0aa3b4e9a44970608
thetpainghtut <thetpainghtut.bf@gmail.com>
2019-09-09 15:28:58 +0630

    update merge conflit with aye chan oo

--- app/Http/Controllers/Api/SectionController.php
+++ app/Http/Controllers/Api/SectionController.php
@@ -196,11 +196,8 @@ class SectionController extends Controller
         // replace days for weekends
         
         $enddate = Carbon::parse($secd->copy()->addDays($weekends)); // 15112019
-<<<<<<< HEAD
+
         // dd($enddate);
-=======
-       // dd($enddate);
->>>>>>> origin/Aco-mmit-proj
         
         // Find weekends from secd
         $weekends = $secd->diffInDaysFiltered(function(Carbon $date) {
